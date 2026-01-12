39774932990748e5293c5168eee60a00b28fee53
Shaikh Mohammad Raza <mohammadrazashaikh59@gmail.com>
2026-01-12 16:48:53 +0530

    db fixing1

--- app/config.py
+++ app/config.py
@@ -8,13 +8,15 @@ class Config:
 
     DATABASE_URL = os.getenv("DATABASE_URL")
 
-   
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgres://",
-            "postgresql+psycopg2://",
-            1
-        )
+    if DATABASE_URL:
+        DATABASE_URL = DATABASE_URL.strip()  # 🔴 REMOVE NEWLINES/SPACES
+
+        if DATABASE_URL.startswith("postgres://"):
+            DATABASE_URL = DATABASE_URL.replace(
+                "postgres://",
+                "postgresql+psycopg2://",
+                1
+            )
 
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
