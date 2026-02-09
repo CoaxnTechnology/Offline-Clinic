48032e94e9b7a27a437e29b91e0f4e03e43084ac
Shaikh Mohammad Raza <mohammadrazashaikh59@gmail.com>
2026-02-09 16:26:49 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .patient import Patient
-from .appointment import Appointment
-from .admin import Admin
-from .dicom import DicomImage, DicomMeasurement
-from .report import Report
-
-__all__ = ["Patient", "Appointment", "Admin", "DicomImage", "DicomMeasurement", "Report"]
