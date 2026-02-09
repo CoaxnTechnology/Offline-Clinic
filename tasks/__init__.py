acd130bc6acfa0a16188f9bb5ecce7b85ab7f4ca
Shaikh Mohammad Raza <mohammadrazashaikh59@gmail.com>
2026-02-09 16:27:37 +0530

    Delete tasks directory

--- tasks/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Celery tasks module
-Import all tasks here so Celery can discover them
-"""
-from . import dicom_tasks, report_tasks, sync_tasks
-
-__all__ = ['dicom_tasks', 'report_tasks', 'sync_tasks']
